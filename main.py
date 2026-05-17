0403a6e3006262169d0fcfe699435feb67224140
abhinav-tripathi21 <abtr.06@gmail.com>
2026-05-18 01:08:37 +0530

    main.py

--- main.py
+++ main.py
@@ -1,26 +1,59 @@
 from linkedin_scraper import search_jobs
 from gmail_service import send_email
+import pandas as pd
+from datetime import datetime
+from email_template import generate_email
 
-jobs = search_jobs("Java Developer")
+keywords = [
+    "Java Developer C2C",
+    "Business Analyst C2C",
+    "Project Manager C2C",
+    "Data Analyst C2C"
+]
 
-for job in jobs:
+already_sent = set()
 
-    subject = f"Application for {job['title']}"
+for keyword in keywords:
 
-    body = f"""
-Hello Hiring Team,
+    print("Searching for ", keyword)
 
-I am interested in the {job['title']} role at {job['company']}.
+    jobs = search_jobs(keyword)
 
-Please find my resume attached.
+    df = pd.DataFrame(jobs)
+    df.to_csv("data/jobs.csv", index=False)
 
-Regards,
-NAME
-"""
+    for job in jobs:
 
-    send_email(
-        receiver_email=job["email"],
-        subject=subject,
-        body=body,
-        resume_path="resumes/resume.pdf"
-    )
+        print("JOB FOUND:", job)
+
+        job["applied_at"] = datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+
+        subject, body = generate_email(job)
+
+        if job["email"] not in already_sent:
+
+            print("Sending email to:", job["email"])
+            print(subject)
+            print(body)
+
+            send_email(
+                receiver_email=job["email"],
+                subject=subject,
+                body=body,
+                resume_path="resumes/resume.pdf",
+                cc_emails=[
+                    "quin@jpitstaffing.com",
+                    "kim@jpitstaffing.com"
+                ]
+            )
+
+            already_sent.add(job["email"])
+
+            print("EMAIL SENT SUCCESSFULLY")
+
+        else:
+            print("DUPLICATE EMAIL SKIPPED:", job["email"])
+
+print("\nALL APPLICATIONS SENT")
